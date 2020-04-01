10cb85788e134908927059d7b276ea5844914e83
cheunglei <41486723+cheunglei@users.noreply.github.com>
2020-04-01 16:59:14 +0800

    Update train_synthetic.sh

--- scripts/train_synthetic.sh
+++ scripts/train_synthetic.sh
@@ -5,19 +5,19 @@ source activate LENSR
 ds_names=("0303" "0306" "0606")
 
 ###### General form to CNF, d-DNNF #####
-#cd ../tools
-#for ds_name in ${ds_names[@]}; do
-#    python raw2data.py --save_path ../dataset/Synthetic --ds_name "${ds_name}"
-#done
-#cd -
+cd ../tools
+for ds_name in ${ds_names[@]}; do
+    python raw2data.py --save_path ../dataset/Synthetic --ds_name "${ds_name}"
+done
+cd -
 
 ###### CNF, d-DNNF to GCN compatible #####
-#cd ../tools
-#for ds_name in ${ds_names[@]}; do
-#    python cnf2data.py --save_path ../dataset/Synthetic --ds_name "${ds_name}"
-#    python ddnnf2data.py --save_path ../dataset/Synthetic --ds_name "${ds_name}"
-#done
-#cd -
+cd ../tools
+for ds_name in ${ds_names[@]}; do
+    python cnf2data.py --save_path ../dataset/Synthetic --ds_name "${ds_name}"
+    python ddnnf2data.py --save_path ../dataset/Synthetic --ds_name "${ds_name}"
+done
+cd -
 
 ###### Train Synthetic dataset #####
 cd ../model/gcn
@@ -25,9 +25,9 @@ cd ../model/gcn
 atom_options=("_0606" "_0306" "_0303" )
 dataset_options=( "ddnnf"  )
 seeds=(  "80" "81" "82" "83" "84" )
-#atom_options=("_0606" "_0306" "_0303"  )
-#dataset_options=( "general"   )
-#seeds=( "8"  )
+atom_options=("_0606" "_0306" "_0303"  )
+dataset_options=( "general"   )
+seeds=( "8"  )
 
 
 
@@ -50,8 +50,8 @@ for seed in ${seeds[@]}; do
             if [[ ${dataset} == 'ddnnf' ]]; then
                 python train.py --ds_path ../../dataset/Synthetic --dataset ${dataset}${atom} --epochs 15 --dataloader_worker 0 ${reg_options} ${ind_options} --seed ${seed}
                 python train.py --ds_path ../../dataset/Synthetic --dataset ${dataset}${atom} --epochs 15 --dataloader_worker 0 ${reg_options} --seed ${seed}
-                #python train.py --ds_path ../../dataset/Synthetic --dataset ${dataset}${atom} --epochs 15 --dataloader_worker 0 ${non_reg_options} ${ind_options} --seed ${seed}
-                #python train.py --ds_path ../../dataset/Synthetic --dataset ${dataset}${atom} --epochs 15 --dataloader_worker 0 ${non_reg_options} --seed ${seed}
+                python train.py --ds_path ../../dataset/Synthetic --dataset ${dataset}${atom} --epochs 15 --dataloader_worker 0 ${non_reg_options} ${ind_options} --seed ${seed}
+                python train.py --ds_path ../../dataset/Synthetic --dataset ${dataset}${atom} --epochs 15 --dataloader_worker 0 ${non_reg_options} --seed ${seed}
             fi
         done
     done
