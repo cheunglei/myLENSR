3744430dea5929b81c5ecefe2b18720b4476a9e1
cheunglei <41486723+cheunglei@users.noreply.github.com>
2020-04-01 16:58:28 +0800

    Update train_compared.sh

--- scripts/train_compared.sh
+++ scripts/train_compared.sh
@@ -5,13 +5,13 @@ source activate LENSR
 
 ###### no_logic ######
 cd ../model/relation_prediction_no_logic
-#python train.py --epochs 10 --seed 0
+python train.py --epochs 10 --seed 0
 cd -
 
 ###### semantic_loss ######
 semantic_loss_weight="0.0005"
 cd ../model/relation_prediction_semantic_loss
-#python train.py --epochs 10 --seed 0 --semantic_loss_weight ${semantic_loss_weight}
+python train.py --epochs 10 --seed 0 --semantic_loss_weight ${semantic_loss_weight}
 cd -
 
 ###### tree-lstm ######
@@ -23,16 +23,16 @@ atom_options=( "_ddnnf")
 dataset_options=("vrd")
 seed="0"
 
-#for dataset in ${dataset_options[@]}; do
-#    for atom in "${atom_options[@]}"; do
-#        if [[ "${atom}" == "" ]]; then
-#            python train.py --ds_path ../../dataset/VRD --dataset ${dataset}${atom} --epochs 1 --dataloader_worker 0  --seed ${seed}
-#        fi
-#        if [[ ${atom} == '_ddnnf' ]]; then
-#            python train.py --ds_path ../../dataset/VRD --dataset ${dataset}${atom} --epochs 1 --dataloader_worker 0  --seed ${seed}
-#        fi
-#    done
-#done
+for dataset in ${dataset_options[@]}; do
+    for atom in "${atom_options[@]}"; do
+        if [[ "${atom}" == "" ]]; then
+            python train.py --ds_path ../../dataset/VRD --dataset ${dataset}${atom} --epochs 1 --dataloader_worker 0  --seed ${seed}
+        fi
+        if [[ ${atom} == '_ddnnf' ]]; then
+            python train.py --ds_path ../../dataset/VRD --dataset ${dataset}${atom} --epochs 1 --dataloader_worker 0  --seed ${seed}
+        fi
+    done
+done
 cd -
 
 ###### Train relation predictor ######
